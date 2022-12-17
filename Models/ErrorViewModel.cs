2749d27ccc8e5ec04b52cc1952ab08f5c73c64e1
TCipher <tochind29@gmail.com>
2022-12-17 03:54:37 +0100

    InitialSetup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DecaPlayStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
